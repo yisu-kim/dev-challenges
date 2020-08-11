b0edcf2536490a57b526e1c4752f8f3e47faa667
yisu-kim <yisu.kim.dev@outlook.com>
2020-08-11 17:19:40 +0900

    [#25] Home 페이지 작성
    - App에 Home 페이지 추가
    - 주어진 디자인대로 Button component 표시
    
    Resolves #25

--- button-component/src/App.js
+++ button-component/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
-import "./App.css";
+import Home from "./routes/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ button-component/src/routes/Home.js
@@ -0,0 +1,102 @@
+import React from "react";
+import Button from "../components/Button";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="Home">
+      <header>
+        <h1>Buttons</h1>
+      </header>
+      <section className="contents">
+        <div className="container">
+          <div className="item">
+            <p> &lt;Button /&gt;</p>
+            <Button />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p> &lt;Button variant="outline" /&gt;</p>
+            <Button variant="outline" color="primary" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p> &lt;Button variant="text" /&gt;</p>
+            <Button variant="text" color="primary" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p> &lt;Button disableShadow /&gt;</p>
+            <Button disableShadow color="primary" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p> &lt;Button disabled /&gt;</p>
+            <Button disabled innerText="Disabled" />
+          </div>
+          <div className="item">
+            <p> &lt;Button variant="text" disabled /&gt;</p>
+            <Button variant="text" disabled innerText="Disabled" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p> &lt;Button startIcon="local_grocery_store" /&gt;</p>
+            <Button startIcon="local_grocery_store" color="primary" />
+          </div>
+          <div className="item">
+            <p> &lt;Button endIcon="local_grocery_store" /&gt;</p>
+            <Button endIcon="local_grocery_store" color="primary" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p> &lt;Button size="sm" /&gt;</p>
+            <Button size="sm" color="primary" />
+          </div>
+          <div className="item">
+            <p> &lt;Button size="md" /&gt;</p>
+            <Button size="md" color="primary" />
+          </div>
+          <div className="item">
+            <p> &lt;Button size="lg" /&gt;</p>
+            <Button size="lg" color="primary" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p> &lt;Button color="default" /&gt;</p>
+            <Button color="default" />
+          </div>
+          <div className="item">
+            <p> &lt;Button color="primary" /&gt;</p>
+            <Button color="primary" />
+          </div>
+          <div className="item">
+            <p> &lt;Button color="secondary" /&gt;</p>
+            <Button color="secondary" />
+          </div>
+          <div className="item">
+            <p> &lt;Button color="danger" /&gt;</p>
+            <Button color="danger" />
+          </div>
+        </div>
+      </section>
+      <footer>
+        <div class="attribution">
+          <a href="https://github.com/yisu-kim/dev-challenges/tree/master/my-team-page">
+            yisu-kim
+          </a>
+          <span> @ </span>
+          <a href="https://devchallenges.io/">DevChallenges.io</a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
