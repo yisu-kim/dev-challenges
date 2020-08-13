64006b6fb8f68a6edcf384ca89a415f9ef736f00
yisu-kim <yisu.kim.dev@outlook.com>
2020-08-13 13:55:37 +0900

    [#37] Input 컴포넌트 Home 페이지 작성
    - Home 페이지 컴포넌트 HTML, CSS 작성
    - App 컴포넌트에 Home 추가
    - Google font, material icon link 추가
    
    Resolves #37

---
+++ input-component/src/routes/Home.js
@@ -0,0 +1,106 @@
+import React from "react";
+import "./Home.css";
+import Input from "../components/Input";
+
+function Home() {
+  return (
+    <div className="Home">
+      <header>
+        <h1>Inputs</h1>
+      </header>
+      <section className="contents">
+        <div className="container">
+          <div className="item">
+            <p>&lt;Input /&gt;</p>
+            <Input placeholder="Placeholder" label="Label" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p>&lt;Input error /&gt;</p>
+            <Input error placeholder="Placeholder" label="Label" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p>&lt;Input disabled /&gt;</p>
+            <Input disabled placeholder="Placeholder" label="Label" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p>&lt;Input helperText="Some interesting text" /&gt;</p>
+            <Input
+              helperText="Some interesting text"
+              placeholder="Placeholder"
+              label="Label"
+            />
+          </div>
+          <div className="item">
+            <p>&lt;Input helperText="Some interesting text" error /&gt;</p>
+            <Input
+              helperText="Some interesting text"
+              error
+              placeholder="Placeholder"
+              label="Label"
+            />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p>&lt;Input startIcon /&gt;</p>
+            <Input startIcon="call" placeholder="Placeholder" label="Label" />
+          </div>
+          <div className="item">
+            <p>&lt;Input endIcon /&gt;</p>
+            <Input endIcon="lock" placeholder="Placeholder" label="Label" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p>&lt;Input value="text" /&gt;</p>
+            <Input value="text" placeholder="Placeholder" label="Label" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p>&lt;Input size="sm" /&gt;</p>
+            <Input size="sm" placeholder="Placeholder" label="Label" />
+          </div>
+          <div className="item">
+            <p>&lt;Input size="md" /&gt;</p>
+            <Input size="md" placeholder="Placeholder" label="Label" />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p>&lt;Input fullWidth /&gt;</p>
+            <Input
+              fullWidth
+              value="text"
+              placeholder="Placeholder"
+              label="Label"
+            />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <p>&lt;Input multiline row="4" /&gt;</p>
+            <Input row="4" placeholder="Placeholder" label="Label" />
+          </div>
+        </div>
+      </section>
+      <footer>
+        <div className="attribution">
+          <a href="https://github.com/yisu-kim/dev-challenges/tree/master/input-component">
+            yisu-kim
+          </a>
+          <span> @ </span>
+          <a href="https://devchallenges.io/">DevChallenges.io</a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
