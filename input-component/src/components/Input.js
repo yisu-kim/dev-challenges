a05d38a8511f1386a82fcd54d672b7230eb2af77
yisu-kim <yisu.kim.dev@outlook.com>
2020-08-13 16:51:09 +0900

    [#41] focus state 관리
    - Input 컴포넌트를 class 컴포넌트로 전환
    - state에 isFocus 생성
    - handleFocus 구현
    - focus 관련 CSS 작성
    
    Resolves #41

--- input-component/src/components/Input.js
+++ input-component/src/components/Input.js
@@ -1,57 +1,91 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./Input.css";
 
-function Input({
-  name,
-  placeholder,
-  value,
-  error,
-  disabled,
-  helperText,
-  startIcon,
-  endIcon,
-  size,
-  fullWidth,
-  row,
-  label,
-}) {
-  const inputClass = [`size--${size}`, fullWidth && "fullWidth"]
-    .filter((e) => e)
-    .join(" ");
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFocus: false,
+    };
 
-  const inputProps = {
-    name,
-    placeholder,
-    value,
-    className: inputClass,
-    disabled,
-    rows: row,
-  };
+    this.handleFocus = this.handleFocus.bind(this);
+  }
 
-  return (
-    <div
-      className={["input-container", error && "error", disabled && "disabled"]
-        .filter((e) => e)
-        .join(" ")}
-    >
-      {label && <label for="text">{label}</label>}
-      <div className="input-area">
-        {startIcon && (
-          <i className="material-icons icon--startIcon">{startIcon}</i>
-        )}
-        {row ? (
-          <textarea id="text" {...inputProps}>
-            {value}
-          </textarea>
-        ) : (
-          <input id="text" type="text" {...inputProps}></input>
-        )}
-        {endIcon && <i className="material-icons icon--endIcon">{endIcon}</i>}
+  handleFocus() {
+    this.setState({
+      isFocus: !this.state.isFocus,
+    });
+  }
+
+  render() {
+    const {
+      name,
+      placeholder,
+      value,
+      error,
+      disabled,
+      helperText,
+      startIcon,
+      endIcon,
+      size,
+      fullWidth,
+      row,
+      label,
+    } = this.props;
+    const inputClass = [`size--${size}`, fullWidth && "fullWidth"]
+      .filter((e) => e)
+      .join(" ");
+
+    const inputProps = {
+      name,
+      placeholder,
+      value,
+      className: inputClass,
+      disabled,
+      rows: row,
+    };
+
+    return (
+      <div
+        className={[
+          "input-container",
+          error && "error",
+          disabled && "disabled",
+          this.state.isFocus && "focus",
+        ]
+          .filter((e) => e)
+          .join(" ")}
+      >
+        {label && <label for="text">{label}</label>}
+        <div className="input-area">
+          {startIcon && (
+            <i className="material-icons icon--startIcon">{startIcon}</i>
+          )}
+          {row ? (
+            <textarea
+              id="text"
+              {...inputProps}
+              onFocus={this.handleFocus}
+              onBlur={this.handleFocus}
+            >
+              {value}
+            </textarea>
+          ) : (
+            <input
+              id="text"
+              type="text"
+              {...inputProps}
+              onFocus={this.handleFocus}
+              onBlur={this.handleFocus}
+            ></input>
+          )}
+          {endIcon && <i className="material-icons icon--endIcon">{endIcon}</i>}
+        </div>
+        {helperText && <span className="helper-text">{helperText}</span>}
       </div>
-      {helperText && <span className="helper-text">{helperText}</span>}
-    </div>
-  );
+    );
+  }
 }
 
 Input.propTypes = {
